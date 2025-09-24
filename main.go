c5e59ccf395a8529a6d73e66a8fea534cb8ea2f7
aabbuukkaarr8 <aabbuukkaarr8928@gmail.com>
2025-09-24 20:29:35 +0300

    finish

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/aabbuukkaarr8/anagrams"
+	"sort"
+	"strings"
 )
 
 func main() {
 	words := []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик", "стол"}
-	res := anagrams.FindAnagrams(words)
+	res := FindAnagrams(words)
 	for k, group := range res {
 		fmt.Println(k, "->", group)
 	}
+}
+
+func FindAnagrams(words []string) map[string][]string {
+
+	anagramGroups := make(map[string][]string)
+
+	for _, w := range words {
+		word := strings.ToLower(w)
+		letters := []rune(word)
+		sort.Slice(letters, func(i, j int) bool { return letters[i] < letters[j] })
+		key := string(letters)
+		anagramGroups[key] = append(anagramGroups[key], word)
+	}
+
+	result := make(map[string][]string)
+	for _, group := range anagramGroups {
+		if len(group) < 2 {
+			continue
+		}
+		sort.Strings(group)
+		first := group[0]
+		result[first] = group
+	}
+
+	return result
 }
